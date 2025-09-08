aea4d128622918ffe789e7f59b67fd2097c01922
Rafał Piekara <rafpiek@gmail.com>
2025-09-08 13:19:31 +0200

    Initial Commit

---
+++ silkaTests/silkaTests.swift
@@ -0,0 +1,16 @@
+//
+//  silkaTests.swift
+//  silkaTests
+//
+//  Created by Rafał Piekara on 08/09/2025.
+//
+
+import Testing
+
+struct silkaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
